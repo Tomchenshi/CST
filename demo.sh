510138e985f2effc3155ec7942b435306a5aecfb
Tomchenshi <50409097+Tomchenshi@users.noreply.github.com>
2023-09-11 21:18:40 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,6 +1,5 @@
-python main38.py test --model_title "HSIformer38_window216_32_0.3_0.1" --dataset "Chikusei" --n_scale 2 --gpus "3"
+python main_CST.py train --dataset "Chikusei" --n_scale 4 --gpus "0,1"
 
-python main38.py test --model_title "HSIformer38_window216_32_0.3_0.1" --dataset "Houston" --n_scale 2 --gpus "3"
+python main_CST.py train --dataset "Houston" --n_scale 4 --gpus "0,1"
 
-python main38.py test --model_title "HSIformer38_window216_32_0.3_0.1" --dataset "Pavia" --n_scale 2 --gpus "3"
-
+python main_CST.py train --dataset "Pavia" --n_scale 4 --gpus "0,1"
